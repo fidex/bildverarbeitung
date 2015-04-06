fa8a3d8510f31b8389752dbf48f1869fa96045ee
fidex <fide.schuermeyer@hs-osnabrueck.de>
2015-04-06 23:50:05 +0200

    9

---
+++ bildverarbeitung_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fidex
+ *
+ * Created on 6. April 2015, 23:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
